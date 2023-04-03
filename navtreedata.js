4afb2990ab30748432cde99924836af9e276177e
cjcchen <cjcchen@users.noreply.github.com>
2023-04-03 09:57:15 +0000

    deploy: 8b358c62fba63c1a16b9477149e780385038ba4b

---
+++ navtreedata.js
@@ -0,0 +1,81 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "ResilientDB", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d5b/classresdb__validator_1_1transaction_1_1TransactionLink.html#a10e7faa7f909130d180ca8d8c0d90efb",
+"d3/d81/utxo__client__tools_8cpp.html#a1e3946030636ad02e7ff9413dcb1656b",
+"d5/d10/namespaceresdb.html#a690655c52d6c71dcf5e135b1bfe3f96a",
+"d6/d47/kv__server_2kv__server__performance_8cpp.html#a23749c714e816c5f22aaed0718ee0a8d",
+"d8/d15/classresdb_1_1utxo_1_1Transaction.html#a4905ab43d66b58cb9ef5bafc3594153d",
+"d9/dfa/classresdb_1_1contract_1_1ContractClient.html#a7d414fceddccb9146ceff3d34198b96c",
+"db/ddb/classresdb_1_1ResDBConfig.html#a90b6b79ce297e988e53c26f380c50c0a",
+"dd/d07/geo__pbft__commitment__test_8cpp_source.html",
+"de/d0d/classresdb_1_1Stats.html#a16b7ccdd8a90a554b1e2161f66d3c650",
+"df/d01/classresdb_1_1TransactionManager.html#a825b0fde2f6094d0d40cec9ba6556154",
+"dir_85effdddc426fd82e3610be0948fd413.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/namespaces_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deploy_5fmanager_0',['deploy_manager',['../de/d8d/namespacedeploy__manager.html',1,'']]]
+];
