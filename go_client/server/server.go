6b2f7aa86d83ebc7965c64e55595e1c8d78e9263
Ubuntu <ubuntu@ip-172-31-23-88.ec2.internal>
2023-04-29 18:35:53 +0000

    fix

--- go_client/server/server.go
+++ go_client/server/server.go
@@ -153,7 +153,7 @@ func (s*Service) GetTransaction(min_seq uint64, max_seq uint64) (buf []byte){
           t = t+1
         }
       }
-      log.Print("check data",i," num:",t)
+      log.Print("check data",uint64(i) + min_seq," num:",t)
       if t == 10 {
         log.Print("no data:",uint64(i) + min_seq)
         continue
